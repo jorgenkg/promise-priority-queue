c2f5bf343bf118175acb339d3f360a25ee8fab9b
Jørgen Grimnes <jorgen@bldng.ai>
2021-07-15 09:54:51 +0200

    chore: cleaned up tests

--- test/integration/tests.ts
+++ test/integration/tests.ts
@@ -9,12 +9,15 @@ async function timeout<T>(fnOrPromise: Promise<T> | (() => Promise<T>), ms = 100
     ]);
 }
 
+function createTask<T>(returns: T, delayMs = 100): () => Promise<T> {
+  return async() => await new Promise<T>(resolve => setTimeout(() => resolve(returns), delayMs));
+}
+
 
 test("It should be possible to add tasks to the queue and return the promised result of the task", async t => {
   const queue = new PromiseQueue();
   const result = "result value";
-  const taskFullfillmentPromise = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve(result), 100)));
-  t.ok(taskFullfillmentPromise instanceof Promise, "Expected the return value to be a promise");
+  const taskFullfillmentPromise = queue.addTask(0, createTask(result));
   t.equal(await timeout(taskFullfillmentPromise), result, "Expected the promise returned by addTask to resolve to the result from the task");
 });
 
@@ -23,7 +26,7 @@ test("It should be possible to add tasks to the queue when it is paused without 
   const queue = new PromiseQueue();
   queue.pause();
 
-  const taskFullfillmentPromise = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve(true), 100)));
+  const taskFullfillmentPromise = queue.addTask(0, createTask(true));
 
 
   const executedTask = await Promise
@@ -39,8 +42,8 @@ test("It should execute tasks with highest priority first", async t => {
   const queue = new PromiseQueue(2, 1);
   queue.pause();
 
-  const taskA = queue.addTask(1, async() => await new Promise(resolve => setTimeout(() => resolve("A"), 100)));
-  const taskB = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("B"), 200)));
+  const taskA = queue.addTask(1, createTask("A", 100));
+  const taskB = queue.addTask(0, createTask("B", 200));
 
   queue.resume();
 
@@ -57,8 +60,8 @@ test("It should execute tasks with identical priority in a FIFO manner", async t
   const queue = new PromiseQueue(1, 1);
   queue.pause();
 
-  const taskA = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("A"), 200)));
-  const taskB = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("B"), 100)));
+  const taskA = queue.addTask(0, createTask("A", 200));
+  const taskB = queue.addTask(0, createTask("B", 100));
 
   queue.resume();
 
@@ -75,8 +78,8 @@ test("Tasks should be concurrently executable if 'concurrency' > 1", async t => 
   const queue = new PromiseQueue(1, 2);
   queue.pause();
 
-  const taskA = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("A"), 200)));
-  const taskB = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("B"), 200)));
+  const taskA = queue.addTask(0, createTask("A", 200));
+  const taskB = queue.addTask(0, createTask("B", 200));
 
   queue.resume();
 
@@ -94,8 +97,8 @@ test("Tasks should not be executed concurrently if 'concurrency' = 1", async t =
   const queue = new PromiseQueue(1, 1);
   queue.pause();
 
-  const taskA = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("A"), 200)));
-  const taskB = queue.addTask(0, async() => await new Promise(resolve => setTimeout(() => resolve("B"), 200)));
+  const taskA = queue.addTask(0, createTask("A", 200));
+  const taskB = queue.addTask(0, createTask("B", 200));
 
   queue.resume();
 
@@ -112,7 +115,7 @@ test("Tasks should not be executed concurrently if 'concurrency' = 1", async t =
 test("It should gracefully handle errors thrown in tasks by rejecting the task promise", async t => {
   const queue = new PromiseQueue(1, 2);
 
-  const task = queue.addTask(0, async() => await new Promise((resolve, reject) => setTimeout(() => reject(), 10)));
+  const task = queue.addTask(0, async() => await new Promise((resolve, reject) => setTimeout(reject, 10)));
 
   t.ok(await task.then(() => false).catch(() => true), "Expected the failing task to be rejected");
 });
